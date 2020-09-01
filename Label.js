68760ccffeb3a6a4854e38d4e59044c478ea961b
Saja-alawneh <35113490+Saja-alawneh@users.noreply.github.com>
2020-08-31 20:13:12 -0400

    Add files via upload

--- Label.js
+++ Label.js
@@ -1,162 +1,207 @@
-
 const _ = require('lodash');
 const fs = require('fs');
+// declare the four quarters days range
 var q1=90;
 var q2=180;
 var q3=270;
 var q4=365;
-//read the JSON file for Patient 1
-let rawdata = require("./data-P1.json");
+
+totalFindingQ1= 0;
+totalDisorderQ1=0;
+totalDrugQ1=0;
+totalLabQ1=0;
+totalProcdureQ1=0;
+totalOthersQ1=0;
+
+totalFindingQ2= 0;
+totalDisorderQ2=0;
+totalDrugQ2=0;
+totalLabQ2=0;
+totalProcdureQ2=0;
+totalOthersQ2=0;
+
+totalFindingQ3= 0;
+totalDisorderQ3=0;
+totalDrugQ3=0;
+totalLabQ3=0;
+totalProcdureQ3=0;
+totalOthersQ3=0;
+
+totalFindingQ4= 0;
+totalDisorderQ4=0;
+totalDrugQ4=0;
+totalLabQ4=0;
+totalProcdureQ4=0;
+totalOthersQ4=0;
+
+//read the JSON file for aal the Patients
+let rawdata = require("./data.json");
 let data= JSON.parse(JSON.stringify(rawdata));
-item = data[0] 
+
+item = data[0]
 key = Object.keys(item)[0]
-//Store patient 1 data into an array called patientdata
+//Store patients data into an array called patientdata
 patientdata= item[key]
-//sort the patientdata by the documentDate 
-patientDataSorted=_.sortBy(patientdata, ['documentDate']);
-
-console.log('Sorted data',patientDataSorted );
 
 
-
-//documentDate is a string. Convert documnetDate which is a string to a date variable
-for( j = 0; j < patientDataSorted.length; j++){
-    patientDataSorted[j]['NewDate']=new Date (patientDataSorted[j]['documentDate']);
-    console.log (patientDataSorted[j]['documentDate'],'  ',patientDataSorted[j]['NewDate']);
+//documentDate is a string. Convert documnetDate which is a string to a date object
+// I added an element to the array called  NewDate to store the date object for each document
+for( j = 0; j < patientdata.length; j++){
+    patientdata[j]['NewDate']=new Date (patientdata[j]['documentDate']);
+    
+    //To view all the NewDate for all the documents for each patient in the cohort 
+    //console.log(patientdata[j]['patientId'],' ', patientdata[j]['documentDate'], ' ', patientdata[j]['NewDate'])
 }
-    //Define a new array to store the patientId, Year, Quarter, labels counts per quarter.
-    let patientLabels =[];
-    
-    // set the start date of the timeline of the sorted array
-    patientDataSorted[0]['Year']= 'Y1';
-    patientDataSorted[0]['Quarter']= 'Q1';
-    
-    // define variabels to count labels for each quarter
-    totalFindingQ1= 0;
-    totalDisorderQ1=0;
-    totalDrugQ1=0;
-    totalLabQ1=0;
-    totalProcdureQ1=0;
-    totalOthersQ1=0;
+//sort the patientdata by the new date object 
 
-    totalFindingQ2= 0;
-    totalDisorderQ2=0;
-    totalDrugQ2=0;
-    totalLabQ2=0;
-    totalProcdureQ2=0;
-    totalOthersQ2=0;
+patientDataSorted=_.sortBy(patientdata, ['NewDate']);
+// This  to check if the patientDataSorted has the documents sorted by the NewDate 
+//for( j = 0; j < patientDataSorted.length; j++){
+  //  console.log(patientDataSorted[j]['patientId'],' ', patientdata[j]['NewDate'], ' ', patientDataSorted[j]['NewDate']);
+//} 
 
-    totalFindingQ3= 0;
-    totalDisorderQ3=0;
-    totalDrugQ3=0;
-    totalLabQ3=0;
-    totalProcdureQ3=0;
-    totalOthersQ3=0;
-
-    totalFindingQ4= 0;
-    totalDisorderQ4=0;
-    totalDrugQ4=0;
-    totalLabQ4=0;
-    totalProcdureQ4=0;
-    totalOthersQ4=0;
-    //Calculate difference between the start document date and the other documents
-    // This will calculate the number of Years  assign each document to the Year and the quarter in that year
-    for( i = 1; i < patientDataSorted.length; i++){
-    var diff = Math.abs((patientDataSorted[0]['NewDate']- patientDataSorted[i]['NewDate'])/86400000);
-    var diff1= Math.floor(diff);
-    var docYear= Math.ceil(diff1/365);
-    let y = docYear;
-    while (y >= 1)
-        {
-            patientDataSorted[i]['Year']= 'Y' + docYear;
-           // patientLabels[i]['Year']='Y' + docYear;
+// Group the data using patientId
+var patientGroups = _(patientDataSorted)
+            .groupBy(x => x.patientId)
+            .map((value, key ) => ({patientId: key, data: value}))
+            .value();
             
+   // console.log(patientGroups)
+
+// To detemine the Year and Quarter for each document for each patient in the cohort
+
+for (i= 0; i< patientGroups.length; i++)
+{
+    for (j=1; j<patientGroups[i].data.length; j++)
+    {
+        patientGroups[i].data[0]['Year']= 'Y1'; // first document for each patient is the starting date (Y1 and Q1)
+        patientGroups[i].data[0]['Quarter']= 'Q1';
+        //substract each document date from the start date to align all the documents date for each patient according to the starting point
+        var diff = Math.abs((patientGroups[i].data[0]['NewDate']- patientGroups[i].data[j]['NewDate'])/86400000);
+        var diff1= Math.floor(diff);
+        var docYear= Math.ceil(diff1/365); //to calculate the number of years where the documents distributed for each patient
+        
+        let y = docYear;
+        while (y >= 1)
+        {
+            patientGroups[i].data[j]['Year']= 'Y' + docYear; 
             y--;
         }
-        
-        // to calculate the quarter 
+    
+        //to calculate the quarter 
         // example : if the document is located in Year 2 and the difference between the first date and the current date is 488 days
         // Then to assign the quarter, I will use the following formula
         // docQuarter = Abs[(2-1)*365 - 488] = 123 (Quarter 2)
         docQuarter= Math.abs((docYear-1) * 365 -diff1);
-        console.log('Quarter are ')
-        console.log(docQuarter);
-        // to assign the quarter and calculate  each label counts per quarter
-        // Add them to the patientLabels array
+
         if (docQuarter <= q1){
-        patientDataSorted[i]['Quarter']= 'Q1';
-        patientDataSorted[i]['totalFindingQ1']= totalFindingQ1 + patientDataSorted[i]['labelCounts']['FindingCount'];
-        patientDataSorted[i]['totalDisorderQ1']= totalDisorderQ1 + patientDataSorted[i]['labelCounts']['DisorderCount'];
-        patientDataSorted[i]['totalDrugQ1']= totalDrugQ1 + patientDataSorted[i]['labelCounts']['DrugCount'];
-        patientDataSorted[i]['totalLabQ1']= totalLabQ1 + patientDataSorted[i]['labelCounts']['LabCount'];
-        patientDataSorted[i]['totalProcdureQ1']= totalProcdureQ1 + patientDataSorted[i]['labelCounts']['ProcedureCount'];
-        patientDataSorted[i]['totalOthersQ1']= totalOthersQ1 + patientDataSorted[i]['labelCounts']['OtherCount'];
-        
+            patientGroups[i].data[j]['Quarter']= 'Q1';
         }
         else if (docQuarter > q1 && docQuarter<= q2)
         {
-        patientDataSorted[i]['Quarter']= 'Q2';
-        patientDataSorted[i]['totalFindingQ2']= totalFindingQ2 + patientDataSorted[i]['labelCounts']['FindingCount'];
-        patientDataSorted[i]['totalDisorderQ2']= totalDisorderQ2 + patientDataSorted[i]['labelCounts']['DisorderCount'];
-        patientDataSorted[i]['totalDrugQ2']= totalDrugQ2 + patientDataSorted[i]['labelCounts']['DrugCount'];
-        patientDataSorted[i]['totalLabQ2']= totalLabQ2 + patientDataSorted[i]['labelCounts']['LabCount'];
-        patientDataSorted[i]['totalProcdureQ2']= totalProcdureQ2 + patientDataSorted[i]['labelCounts']['ProcedureCount'];
-        patientDataSorted[i]['totalOthersQ2']= totalOthersQ2 + patientDataSorted[i]['labelCounts']['OtherCount'];
+            patientGroups[i].data[j]['Quarter']= 'Q2';
         }
         
         else if (docQuarter > q2 && docQuarter<= q3)
         {
-        patientDataSorted[i]['Quarter']= 'Q3';
-        patientDataSorted[i]['totalFindingQ3']= totalFindingQ3 + patientDataSorted[i]['labelCounts']['FindingCount'];
-        patientDataSorted[i]['totalDisorderQ3']= totalDisorderQ3 + patientDataSorted[i]['labelCounts']['DisorderCount'];
-        patientDataSorted[i]['totalDrugQ3']= totalDrugQ3 + patientDataSorted[i]['labelCounts']['DrugCount'];
-        patientDataSorted[i]['totalLabQ3']= totalLabQ3 + patientDataSorted[i]['labelCounts']['LabCount'];
-        patientDataSorted[i]['totalProcdureQ3']= totalProcdureQ3 + patientDataSorted[i]['labelCounts']['ProcedureCount'];
-        patientDataSorted[i]['totalOthersQ3']= totalOthersQ3 + patientDataSorted[i]['labelCounts']['OtherCount'];
-        
+            patientGroups[i].data[j]['Quarter']= 'Q3';
         }
         else 
         {
-        patientDataSorted[i]['Quarter']= 'Q4';
-        patientDataSorted[i]['totalFindingQ4']= totalFindingQ4 + patientDataSorted[i]['labelCounts']['FindingCount'];
-        patientDataSorted[i]['totalDisorderQ4']= totalDisorderQ4 + patientDataSorted[i]['labelCounts']['DisorderCount'];
-        patientDataSorted[i]['totalDrugQ4']= totalDrugQ4 + patientDataSorted[i]['labelCounts']['DrugCount'];
-        patientDataSorted[i]['totalLabQ4']= totalLabQ4 + patientDataSorted[i]['labelCounts']['LabCount'];
-        patientDataSorted[i]['totalProcdureQ4']= totalProcdureQ4 + patientDataSorted[i]['labelCounts']['ProcedureCount'];
-        patientDataSorted[i]['totalOthersQ4']= totalOthersQ4 + patientDataSorted[i]['labelCounts']['OtherCount'];
-        
-        }
+            patientGroups[i].data[j]['Quarter']= 'Q4';}
+
+
     }
-    console.log(patientDataSorted);
+}
+//Calculate total number of years in the cohort
+var totalDaysDiff= Math.abs((patientDataSorted[0]['NewDate']- patientDataSorted[patientDataSorted.length-1]['NewDate'])/86400000);
+var totalNumYear= Math.ceil(totalDaysDiff/365);
+console.log('first Date in the cohort:', patientDataSorted[0]['NewDate'])
+console.log('Last Date in the cohort:', patientDataSorted[patientDataSorted.length-1]['NewDate'])
+console.log ('number of Days:', totalDaysDiff);
+console.log('number of years:', totalNumYear);
 
-    for( j = 0; j < patientDataSorted.length; j++){
-        if ( patientDataSorted[j].totalFindingQ1 !=undefined) {
-            patientLabels.push({patientId: patientDataSorted[j].patientId}, {Year: patientDataSorted[j]['Year']},
-            {Quarter:patientDataSorted[j]['Quarter']}, {totalFindingQ1:patientDataSorted[j]['totalFindingQ1']})
-        }
-        if ( patientDataSorted[j].totalDrugQ1 !=undefined) {
-            patientLabels.push({patientId: patientDataSorted[j]['patientId']}, {Year: patientDataSorted[j]['Year']},
-            {Quarter:patientDataSorted[j]['Quarter']}, {totalDrugQ1:patientDataSorted[j]['totalDrugQ1']})
-            }
-        if ( patientDataSorted[j].totalDisorderQ1 !=undefined) {
-            patientLabels.push({patientId: patientDataSorted[j]['patientId']}, {Year: patientDataSorted[j]['Year']},
-            {Quarter:patientDataSorted[j]['Quarter']}, {totalDisorderQ1:patientDataSorted[j]['totalDisorderQ1']})
-            }
-        if ( patientDataSorted[j].totalLabQ1 !=undefined) {
-            patientLabels.push({patientId: patientDataSorted[j]['patientId']}, {Year: patientDataSorted[j]['Year']},
-            {Quarter:patientDataSorted[j]['Quarter']}, {totalLabQ1:patientDataSorted[j]['totalLabQ1']})
-            }
+//console.log(patientGroups[1].data)
 
-        if ( patientDataSorted[j].totalProcedureQ1 !=undefined) {
-            patientLabels.push({patientId: patientDataSorted[j]['patientId']}, {Year: patientDataSorted[j]['Year']},
-            {Quarter:patientDataSorted[j]['Quarter']}, {totalProcedureQ1:patientDataSorted[j]['totalProcedureQ1']})
+ let lableCountsYear=[];
+ 
+// Here where I need your help 
+// to aggregate  all documents for each patient, 
+// the goal here is to calculate the counts for each semnatic group (Finding, Procedure, Drug, Disorder, Lab, Others) in each quarter/year 
+ 
+for(y=1; y<totalNumYear; y++) // Loop through the number of years in the cohort
+{
+    for(i=0; i<patientGroups.length; i++) //loop through the patients (47 patients)
+    {
+        for(j=0;j<data.length; j++)// loop through the documents for each patients
+        {
+            if(patientGroups[i].data[j]['Year'].includes('Y'+ y) && patientGroups[i].data[j]['Quarter'].includes('Q1'))
+            {   
+                patientGroups[i]['totalFindingQ1']= totalFindingQ1 + patientGroups[i].data[j]['labelCounts']['FindingCount'];
+                patientGroups[i]['totalDisorderQ1']= totalDisorderQ1 + patientGroups[i].data[j]['labelCounts']['DisorderCount'];
+                patientGroups[i]['totalDrugQ1']= totalDrugQ1 + patientGroups[i].data[j]['labelCounts']['DrugCount'];
+                patientGroups[i]['totalLabQ1']= totalLabQ1 + patientGroups[i].data[j]['labelCounts']['LabCount'];
+                patientGroups[i]['totalProcdureQ1']= totalProcdureQ1 + patientGroups[i].data[j]['labelCounts']['ProcedureCount'];
+                patientGroups[i]['totalOthersQ1']= totalOthersQ1 + patientGroups[i].data[j]['labelCounts']['OtherCount'];
+        
             }
-        if ( patientDataSorted[j].totalOtherQ1 !=undefined) {
-                patientLabels.push({patientId: patientDataSorted[j]['patientId']}, {Year: patientDataSorted[j]['Year']},
-                {Quarter:patientDataSorted[j]['Quarter']}, {totalOtherQ1:patientDataSorted[j]['totalOthersQ1']})
+            else if (patientGroups[i].data[j]['Year'].includes('Y'+ y) && patientGroups[i].data[j]['Quarter'].includes(Q2))
+            {
+        
+                patientGroups[i]['totalFindingQ2']= totalFindingQ2 + patientGroups[i].data[j]['labelCounts']['FindingCount'];
+                patientGroups[i]['totalDisorderQ2']= totalDisorderQ2 + patientGroups[i].data[j]['labelCounts']['DisorderCount'];
+                patientGroups[i]['totalDrugQ2']= totalDrugQ2 + patientGroups[i].data[j]['labelCounts']['DrugCount'];
+                patientGroups[i]['totalLabQ2']= totalLabQ2 + patientGroups[i].data[j]['labelCounts']['LabCount'];
+                patientGroups[i]['totalProcdureQ2']= totalProcdureQ2 + patientGroups[i].data[j]['labelCounts']['ProcedureCount'];
+                patientGroups[i]['totalOthersQ2']= totalOthersQ2 + patientGroups[i].data[j]['labelCounts']['OtherCount'];
+                
+            }
+        
+            else if (patientGroups[i].data[j]['Year'].includes('Y'+ y) && patientGroups[i].data[j]['Quarter'].includes(Q3))
+            {
+                patientGroups[i]['totalFindingQ3']= totalFindingQ3 + patientGroups[i].data[j]['labelCounts']['FindingCount'];
+                patientGroups[i]['totalDisorderQ3']= totalDisorderQ3 + patientGroups[i].data[j]['labelCounts']['DisorderCount'];
+                patientGroups[i]['totalDrugQ3']= totalDrugQ3 + patientGroups[i].data[j]['labelCounts']['DrugCount'];
+                patientGroups[i]['totalLabQ3']= totalLabQ3 + patientGroups[i].data[j]['labelCounts']['LabCount'];
+                patientGroups[i]['totalProcdureQ3']= totalProcdureQ3 + patientGroups[i].data[j]['labelCounts']['ProcedureCount'];
+                patientGroups[i]['totalOthersQ3']= totalOthersQ3 + patientGroups[i].data[j]['labelCounts']['OtherCount'];
+        
+            }
+            else 
+            {
+                patientGroups[i]['totalFindingQ4']= totalFindingQ4 + patientGroups[i].data[j]['labelCounts']['FindingCount'];
+                patientGroups[i]['totalDisorderQ4']= totalDisorderQ4 + patientGroups[i].data[j]['labelCounts']['DisorderCount'];
+                patientGroups[i]['totalDrugQ4']= totalDrugQ4 + patientGroups[i].data[j]['labelCounts']['DrugCount'];
+                patientGroups[i]['totalLabQ4']= totalLabQ4 + patientGroups[i].data[j]['labelCounts']['LabCount'];
+                patientGroups[i]['totalProcdureQ4']= totalProcdureQ4 + patientGroups[i].data[j]['labelCounts']['ProcedureCount'];
+                patientGroups[i]['totalOthersQ4']= totalOthersQ4 + patientGroups[i].data[j]['labelCounts']['OtherCount'];
+        
                 }
             }
+        
+        
+       lableCountsYear.push ({Year: 'Y'+y}, { totalFindingQ1: patientGroups[i]['totalFindingQ1']}, 
+        {totalDisorderQ1: patientGroups[i]['totalDisorderQ1']}, {totalDrugQ1: patientGroups[i]['totalDrugQ1'] }, 
+        {totalLabQ1: patientGroups[i]['totalLabQ1']}, {totalProcdureQ1: patientGroups[i]['totalProcdureQ1']}, 
+        {totalOthersQ1: patientGroups[i]['totalOthersQ1'] }, {totalFindingQ2:patientGroups[i]['totalFindingQ2']},
+        {totalDisorderQ2: patientGroups[i]['totalDisorderQ2']}, {totalDrugQ2: patientGroups[i]['totalDrugQ2']},
+        {totalLabQ2: patientGroups[i]['totalLabQ2']}, {totalProcdureQ2: patientGroups[i]['totalProcdureQ2']},
+        {totalOthersQ2: patientGroups[i]['totalOthersQ2']}, { totalFindingQ3:patientGroups[i]['totalFindingQ3']}, 
+        {totalDisorderQ3: patientGroups[i]['totalDisorderQ3']}, {totalDrugQ3: patientGroups[i]['totalDrugQ3'] }, 
+        {totalLabQ3: patientGroups[i].data[j]['totalLabQ3']}, {totalProcdureQ3: patientGroups[i].data[j]['totalProcdureQ3']}, 
+        {totalOthersQ3: patientGroups[i]['totalOthersQ3'] },{ totalFindingQ4:patientGroups[i]['totalFindingQ4']}, 
+        {totalDisorderQ4: patientGroups[i]['totalOthersQ1']}, {totalDrugQ4: patientGroups[i]['totalDrugQ4'] }, 
+        {totalLabQ4: patientGroups[i]['totalLabQ4']}, {totalProcdureQ4: patientGroups[i]['totalProcdureQ4']}, 
+        {totalOthersQ4: patientGroups[i]['totalOthersQ4'] })
+        }
+     }
+    //I still also need to aggregate all patients for quarter/year
 
-    console.log(patientLabels);
-    //console.log(patientLabels.length)
-    //console.log(JSON.stringify(patientLabels))
+
+
+
+
+
+
+
+    
